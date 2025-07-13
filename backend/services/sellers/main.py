0bea99ea3301d8dc4e72d8f52a2d213084fa77b0
Martin Galvan <md.galvan@uniandes.edu.co>
2025-07-13 18:46:17 -0500

    healtcheck

--- backend/services/sellers/main.py
+++ backend/services/sellers/main.py
@@ -3,13 +3,12 @@ from fastapi import FastAPI, Depends
 
 from config import settings
 from src.infrastructure.adapters.http.health import router as health_router
-from src.infrastructure.adapters.http.routers import router as seller_router
 from src.infrastructure.middlewares.exception_handlers import (
     register_exception_handlers,
 )
+from src.infrastructure.adapters.http.routers import router as seller_router
 from src.infrastructure.logging import setup_logging
 from src.infrastructure.auth import get_current_user
-
 
 
 @asynccontextmanager
@@ -29,7 +28,6 @@ app = FastAPI(
 
 app.include_router(health_router)
 app.include_router(seller_router, dependencies=[Depends(get_current_user)])
-
 
 
 register_exception_handlers(app)

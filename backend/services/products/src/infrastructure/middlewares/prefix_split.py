9946ca1481ce237d9e28ac9dd58303d21768346d
Martin Galvan <md.galvan@uniandes.edu.co>
2025-07-13 00:03:07 -0500

    update split middleware

--- backend/services/products/src/infrastructure/middlewares/prefix_split.py
+++ backend/services/products/src/infrastructure/middlewares/prefix_split.py
@@ -2,17 +2,23 @@ from starlette.types import ASGIApp, Receive, Scope, Send
 
 class StripPrefixMiddleware:
     """
-    ASGI middleware that strips a given prefix from the incoming HTTP path
-    before FastAPI route matching.
+    Strips a given prefix from the path EXCEPT for docs & openapi endpoints.
     """
     def __init__(self, app: ASGIApp, prefix: str):
         self.app = app
         self.prefix = prefix
+        # the paths that we want to preserve under the prefix
+        self._skip = {
+            f"{prefix}/docs",
+            f"{prefix}/openapi.json",
+            f"{prefix}/redoc",
+        }
 
     async def __call__(self, scope: Scope, receive: Receive, send: Send):
         if scope["type"] == "http":
             path = scope["path"]
-            if path.startswith(self.prefix):
+            # only strip prefix if it's not one of the skip paths
+            if path.startswith(self.prefix) and not any(path.startswith(s) for s in self._skip):
                 new_path = path[len(self.prefix):]
                 scope["path"] = new_path or "/"
         await self.app(scope, receive, send)
